a211f449f7c11f17ddb28c6f265c6a93e424c10c
Mickael <mickael.noxalis@gmail.com>
2024-04-08 18:47:34 +0200

    testing ci

--- index.php
+++ index.php
@@ -1,29 +1,27 @@
 <?php
 session_start();
-require_once('database/db.php');
-require_once('security/connexion.php');
+require_once ('database/db.php');
+require_once ('security/connexion.php');
 
-if(isTokenValid($_SESSION['token'])){
+if (isTokenValid($_SESSION['token'])) {
     header("Location: /dashboard.php");
 }
 $error = false;
 $requestMethod = $_SERVER['REQUEST_METHOD'];
 
-    if($requestMethod == 'GET' && $_GET['sub'] == "true"){
-            if(isset($_GET['mail']) && isset($_GET['pass'])){
-                $logUser = loginWithToken($_GET['mail'], $_GET['pass']);
-                if($logUser){
-                    $_SESSION['token'] = $logUser;
-                    header("Location: dashboard.php");
-                }
-                else{
-                    $error = "Connexion impossible, utilisateur non-authentifié";
-                }
-            }
-            else {
-                $error = "Un champ de connexion manquant";
-            }
+if ($requestMethod == 'GET' && $_GET['sub'] == "true") {
+    if (isset($_GET['mail']) && isset($_GET['pass'])) {
+        $logUser = loginWithToken($_GET['mail'], $_GET['pass']);
+        if ($logUser) {
+            $_SESSION['token'] = $logUser;
+            header("Location: dashboard.php");
+        } else {
+            $error = "Connexion impossible, utilisateur non-authentifié";
+        }
+    } else {
+        $error = "Un champ de connexion manquant";
     }
+}
 ?>
 <!DOCTYPE html>
 <html lang="fr">
@@ -39,26 +37,33 @@ $requestMethod = $_SERVER['REQUEST_METHOD'];
 
 <body>
     <div class="container-fluid">
-      <?php
-        if($error){
-          ?><div class="alert alert-danger" role="alert">
-            <?= $error ?>
-          </div>
-          <?php
+        <?php
+        if ($error) {
+            ?>
+            <div class="alert alert-danger" role="alert">
+                <?= $error ?>
+            </div>
+            <?php
         }
-          ?>
+        ?>
         <div class="row mh-100vh">
-            <div class="col-10 col-sm-8 col-md-6 col-lg-6 offset-1 offset-sm-2 offset-md-3 offset-lg-0 align-self-center d-lg-flex align-items-lg-center align-self-lg-stretch bg-white p-5 rounded rounded-lg-0 my-5 my-lg-0" id="login-block">
+            <div class="col-10 col-sm-8 col-md-6 col-lg-6 offset-1 offset-sm-2 offset-md-3 offset-lg-0 align-self-center d-lg-flex align-items-lg-center align-self-lg-stretch bg-white p-5 rounded rounded-lg-0 my-5 my-lg-0"
+                id="login-block">
                 <div class="m-auto w-lg-75 w-xl-50">
                     <h2 class="text-info fw-light mb-5"><i class="fa fa-car"></i>&nbsp;Garage Train</h2>
                     <form method="GET">
-                        <div class="form-group mb-3"><label class="form-label text-secondary">Email</label><input class="form-control" type="text" required="" name="mail" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,15}$" inputmode="email"></div>
-                        <div class="form-group mb-3"><label class="form-label text-secondary">Mot de passe</label><input class="form-control" type="password" name="pass" required=""></div><button class="btn btn-info mt-2" name="sub" type="submit" value="true">Connexion</button>
+                        <div class="form-group mb-3"><label class="form-label text-secondary">Email</label><input
+                                class="form-control" type="text" required="" name="mail"
+                                pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,15}$" inputmode="email"></div>
+                        <div class="form-group mb-3"><label class="form-label text-secondary">Mot de passe</label><input
+                                class="form-control" type="password" name="pass" required=""></div><button
+                            class="btn btn-info mt-2" name="sub" type="submit" value="true">Connexion</button>
                     </form>
                     <p class="mt-3 mb-0"></p>
                 </div>
             </div>
-            <div class="col-lg-6 d-flex align-items-end" id="bg-block" style="background-image: url(&quot;https://images.assetsdelivery.com/compings_v2/auxins/auxins2302/auxins230200611.jpg&quot;);background-size: cover;background-position: center center;">
+            <div class="col-lg-6 d-flex align-items-end" id="bg-block"
+                style="background-image: url(&quot;https://images.assetsdelivery.com/compings_v2/auxins/auxins2302/auxins230200611.jpg&quot;);background-size: cover;background-position: center center;">
                 <p class="ms-auto small text-dark mb-2"><em>Photo by Auxin Nopparat / auxins<br></p>
             </div>
         </div>
